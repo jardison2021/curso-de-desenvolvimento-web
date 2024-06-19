6149107154a83c6e9e3d7c102f1cd239495b83b3
jardison2021 <jardisonrs@live.com>
2024-06-19 20:59:20 -0300

    App Orçamento Pessoal - Parte 2

--- 7. EcmaScript/app_orcamento_pessoal/app.js
+++ 7. EcmaScript/app_orcamento_pessoal/app.js
@@ -1,9 +1,19 @@
+class Despesa{
+	constructor(ano, mes, dia, tipo, descricao, valor){
+		this.ano = ano
+		this.mes = tipo
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+}
 function cadastrarDespesa(){
 	let ano = document.getElementById('ano')
 	let mes = document.getElementById('mes')
 	let dia = document.getElementById('dia')
 	let descricao = document.getElementById('descricao')
 	let valor = document.getElementById('valor')
-
-	console.log(ano.value, mes.value, dia.value, descricao.value, valor.value)
+	let despesa = new Despesa(ano.value, mes.value, dia.value, descricao.value, valor.value)
+	console.log(despesa)
 }
