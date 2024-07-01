4c825f57c3c9b6e36950c00680cfd8e7bedde478
jardison2021 <jardisonrs@live.com>
2024-07-01 20:21:51 -0300

    App Orçamento Pessoal - Parte 4

--- 7. EcmaScript/app_orcamento_pessoal/app.js
+++ 7. EcmaScript/app_orcamento_pessoal/app.js
@@ -8,6 +8,28 @@ class Despesa{
 		this.valor = valor
 	}
 }
+//Classe do objeto BD
+class Bd{
+	constructor(){
+		let id = localStorage.getItem('id')
+		if(id === null){
+			localStorage.setItem('id', 0)
+		}
+	}
+	getProximoId(){
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+	//Função para utilizar o local storage
+	//Esta função permite que toda vez que algum dado seja adiciondo, ela seja setada na notação JSON, artavés do camando JSON.stringify(d), sendo "d" o parâmetro utilizado para guardar as informações.
+	//A função setItem contém um protocolo que abre comunicação com o local storage para o encaminhar o JSON.
+	gravar(d){
+		let id = this.getProximoId()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+	}
+}
+let bd = new Bd()
 function cadastrarDespesa(){
 	let ano = document.getElementById('ano')
 	let mes = document.getElementById('mes')
@@ -15,10 +37,5 @@ function cadastrarDespesa(){
 	let descricao = document.getElementById('descricao')
 	let valor = document.getElementById('valor')
 	let despesa = new Despesa(ano.value, mes.value, dia.value, descricao.value, valor.value)
-	gravar(despesa)
-}
-//Função para utilizar o local storage
-//Esta função permite que toda vez que algum dado seja adiciondo, ela seja setada na notação JSON, artavés do camando JSON.stringify(d), sendo "d" o parâmetro utilizado para guardar as informações.
-function gravar(d){
-	localStorage.setItem('despesa', JSON.stringify(d))
+	bd.gravar(despesa)
 }
